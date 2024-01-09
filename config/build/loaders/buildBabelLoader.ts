fa28ee6592d56b8d8ea9efca02575125d76f7340
Artur Tsebaev <tsebaevartur38@gmail.com>
2024-01-09 23:23:07 +0300

    add babel loader add new babel plugins for build

--- config/build/loaders/buildBabelLoader.ts
+++ config/build/loaders/buildBabelLoader.ts
@@ -1,15 +1,29 @@
+import babelRemovePropsPlugin from '../../babel/babelRemovePropsPlugin';
 import { BuildOptions } from '../types/config';
 
-export function buildBabelLoader(options: BuildOptions) {
+interface buildBabelLoaderProps extends BuildOptions {
+    isTsx?: boolean;
+}
+export function buildBabelLoader({ isDev, isTsx }: buildBabelLoaderProps) {
     return {
-        test: /\.(js|jsx|tsx)$/,
+        test: isTsx ? /\.(jsx|tsx)$/ : /\.(js|ts)$/,
         exclude: /node_modules/,
         use: {
             loader: 'babel-loader',
             options: {
                 presets: ['@babel/preset-env'],
                 plugins: [
-                    options.isDev && require.resolve('react-refresh/babel'),
+                    ['@babel/plugin-transform-typescript', {
+                        isTsx,
+                    }],
+                    '@babel/plugin-transform-runtime',
+                    isTsx && [
+                        babelRemovePropsPlugin,
+                        {
+                            props: ['data-testid'],
+                        },
+                    ],
+                    isDev && require.resolve('react-refresh/babel'),
                 ].filter(Boolean),
             },
         },

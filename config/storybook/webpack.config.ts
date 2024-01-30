22bdfb75ff98362449cce540e7dac009f3a6677d
Artur Tsebaev <tsebaevartur38@gmail.com>
2024-01-30 21:56:25 +0300

    fix configs for jest and storybook

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -9,14 +9,19 @@ export default ({ config }: {config: webpack.Configuration}) => {
         html: '',
         entry: '',
         src: path.resolve(__dirname, '..', '..', 'src'),
-        buildLocales: '',
         locales: '',
+        buildLocales: '',
     };
     config!.resolve!.modules!.push(paths.src);
-    config.resolve!.alias = { ...config!.resolve!.alias, '@': paths.src };
     config!.resolve!.extensions!.push('.ts', '.tsx');
+    config!.resolve!.alias = {
+        ...config!.resolve!.alias,
+        '@': paths.src,
+    };
+
+    // eslint-disable-next-line no-param-reassign
     // @ts-ignore
-    config!.module!.rules = config!.module!.rules!.map((rule: RuleSetRule) => {
+    config!.module!.rules = config.module!.rules!.map((rule: RuleSetRule) => {
         if (/svg/.test(rule.test as string)) {
             return { ...rule, exclude: /\.svg$/i };
         }
